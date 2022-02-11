bbfd77b1ce258e2bbd8f692307c0e464c6c34919
matheusabdias <matheusabdias@b2bit.company>
2022-02-11 16:41:32 -0300

    adicionando um apiview test

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View"""
+    def get(self,request,format=None):
+        """Returna uma lista de funções da APIView"""
+        an_apiview = [
+            'Usando Http metodos (get,post,put,delete,patch)',
+            'É similar a uma tradiciona view do django',
+            'te da o controle da logica da aplicação',
+            'e mapea manualmente as urls',
+        ]
+
+        return Response({'message':'hello','an_apiview':an_apiview})
