24d82e275b64214a93bff2d719858d00facf4d91
matheusabdias <matheusabdias@b2bit.company>
2022-02-14 10:04:46 -0300

    Adiciona API de login

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,9 @@ from rest_framework.response import Response
 from rest_framework import status 
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication 
+from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profiles_api import serializers
 from profiles_api import models
@@ -94,7 +97,16 @@ class HelloApiView(APIView):
         return Response({'methodo':'Delete'})
 
 class UserProfileViewSet(viewsets.ModelViewSet):
+    """Cria e atualiza um usuario """
+     
     serializer_class = serializers.UserProfileSerizalizer
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fileds = ('name','email',)
+
+class UserLoginApiView(ObtainAuthToken):
+    """Cria um token autenticado para o usuario"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+     
